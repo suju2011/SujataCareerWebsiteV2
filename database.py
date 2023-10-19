3a01b2fb4432fff79668a040e81e907f57daceab
suju2011 <106603793+suju2011@users.noreply.github.com>
2023-10-19 13:45:04 +0000

    new changes

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ from sqlalchemy import *
 from sqlalchemy import create_engine, text
 from sqlalchemy. util.langhelpers import dictlike_iteritems
 
-db_connection_string= "mysql+pymysql://dtcicvt7lkedvyvm0is4:pscale_pw_NBPKdK1EzfqDySRIisR52uDTndooOr5TYpV6qAkZsOb@aws.connect.psdb.cloud/sujatacareers?charset=utf8mb4"
+db_connection_string= "mysql+pymysql://9jncxfu9se9pl456yzu3:pscale_pw_HqBfx4MqB4vkAExOgZNnm2snPlj12yQO3sVxF6AWBT0@aws.connect.psdb.cloud/sujatacareers?charset=utf8mb4"
 engine = create_engine(db_connection_string,
                     connect_args={"ssl":{"ssl ca": "/etc/ssl/cert.pem"}})
 
