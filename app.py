57adf090ca1f420ae251f78a7d1f710e75bddd3d
suju2011 <106603793+suju2011@users.noreply.github.com>
2023-10-25 20:43:11 +0000

    App Loaded : load_job_from_db

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db
+from database import load_jobs_from_db,load_job_from_db #this load_job_from_db is another app
 
 # by importing the load_job functin from database file and removing the sqlalchemy from here and the engine also
 
@@ -58,9 +58,24 @@ def hello_sujata():
   return render_template('home.html',jobs=jobs,company_name='SujataCareers')
   # return render_template('home.html',jobs=JOBS,company_name='SujataCareers')
 
-# @app.route("/api/jobs")
-# def list_jobs():
-#   return jsonify(JOBS)
+@app.route("/api/jobs")
+def list_jobs():
+  jobs=load_jobs_from_db()
+  return jsonify(jobs)
   
+# @app.route("/job/<id>")
+# def show_job(id):
+#   job=load_jobs_from_db(id)
+#   return jsonify(job)
+
+
+@app.route("/job/<int:id>")
+def show_job(id):
+    job = load_job_from_db(id)  # Use the correct function to load a specific job
+    if job is None:
+        return "Job not found", 404
+    else:
+        return jsonify(job)
+
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -1,5 +1,3 @@
-
-
 import os
 
 # from sqlalchemy import *
@@ -9,20 +7,34 @@ db_connection_string = os.environ["DB_CONNECTION_STRING"]
 # db_connection_string = os.environ["DB_CONNECTION_STRING"]
 # SETTING THE DATABASE DETAILS SECRATE TO US ONLY and the code should be without double quote so that the Db_Connection_string is in double quote.
 
-
 # db_connection_string= "mysql+pymysql://vb0x6hpuomu9q7d275xq:pscale_pw_imckS4hZZHfbxmn8kcUVdObckpgKHIe1i3Tr39ElUo@aws.connect.psdb.cloud/careers?charset=utf8mb4"
 engine = create_engine(db_connection_string,
-                    connect_args={"ssl":{"ssl ca": "/etc/ssl/cert.pem"}})
+                       connect_args={"ssl": {
+                           "ssl ca": "/etc/ssl/cert.pem"
+                       }})
+
 
 def load_jobs_from_db():
 
   with engine.connect() as conn:
-  
+
     result = conn.execute(text("select * from jobs"))
-   
-    # jobs=[]   
+
+    # jobs=[]
     # for row in result.all():
     #   jobs.append(row._asdict())
     # return jobs
-    jobs=[row._asdict() for row in result.all()]
+    jobs = [row._asdict() for row in result.all()]
     return jobs
+
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from jobs where id= :val"), {"val": id})
+    rows = result.all()
+    
+    if len(rows) == 0:
+      return None
+    else:
+      jobs = [row._asdict() for row in rows]
+      return jobs[0]
