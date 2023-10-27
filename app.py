7a6a61e6bb51356975f006f9613eed36ad667f49
suju2011 <106603793+suju2011@users.noreply.github.com>
2023-10-27 18:41:22 +0000

    Dynamic  Application Apply is activated

--- app.py
+++ app.py
@@ -1,83 +1,34 @@
-from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db,load_job_from_db #this load_job_from_db is another app
+from flask import Flask, jsonify, render_template, request
 
-# by importing the load_job functin from database file and removing the sqlalchemy from here and the engine also
+#this load_job_from_db is another app
+from database import add_application_to_db, load_job_from_db, load_jobs_from_db
 
-# from sqlalchemy import text
 app = Flask(__name__)
-
-# JOBS=[
-#   {
-#     'id':1,
-#     'title':'Data Analyst',
-#     'location':'Bangaluru, India',
-#     'Salary':'Rs. 10,00,000'
-  
-#   },
-#   {
-#     'id':2,
-#     'title':'Data Scientist',
-#     'location':'Delhi, India',
-#     'Salary':'Rs. 15,00,000'
-  
-#   },
-  
-#   {
-#     'id':3,
-#     'title':'FrontEnd Engineer',
-#     'location':'Remote'
-#     # 'Salary':'Rs. 12,00,000'
-  
-#   },
-#   {
-#     'id':4,
-#     'title':'BackEnd Engineer',
-#     'location':'SanFrancisco, USA',
-#     'Salary':'$ 120,00'
-  
-#   }
-  
-  
-# ]
-# Instead of having 2 database file and its details in different file we should call the function from database .py file to import just the function here by importing
-# def load_jobs_from_db():
-
-#   with engine.connect() as conn:
-
-#     result = conn.execute(text("select * from jobs"))
-#     # result_dicts=[]
-#     jobs=[]   # as this is a list of job so replaced the name by job and changed accordingly
-#     for row in result.all():
-#       # result_dicts.append(row._asdict())
-#       jobs.append(row._asdict())
-#     return jobs
 
 @app.route("/")
 def hello_sujata():
   jobs=load_jobs_from_db()
-  return render_template('home.html',jobs=jobs,company_name='SujataCareers')
-  # return render_template('home.html',jobs=JOBS,company_name='SujataCareers')
-
+  return render_template('home.html',jobs=jobs)
 @app.route("/api/jobs")
 def list_jobs():
   jobs=load_jobs_from_db()
   return jsonify(jobs)
-  
-# @app.route("/job/<id>")
-# def show_job(id):
-#   job=load_jobs_from_db(id)
-#   return jsonify(job)
-
-
 @app.route("/job/<int:id>")
 def show_job(id):
     job = load_job_from_db(id)  # Use the correct function to load a specific job
     if job is None:
         return "Job not found", 404
     else:
-       # to pass the dinamic database to use in  render-template we have to use instead of jsonify render tamplate to be used
-      # return jsonify(job)
-      return render_template('jobpage.html', job=job)
+     return render_template('jobpage.html', job=job)
+
+@app.route("/job/<id>/apply", methods=["POST"])
+def apply_to_job(id):
+  data=request.form
+  job=load_job_from_db(id)
+  add_application_to_db(id, data=data)
+  return render_template('application_submitted.html',
+                         application=data,
+                         job=job)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -1,29 +1,17 @@
 import os
 
-# from sqlalchemy import *
 from sqlalchemy import create_engine, text
 
 db_connection_string = os.environ["DB_CONNECTION_STRING"]
-# db_connection_string = os.environ["DB_CONNECTION_STRING"]
-# SETTING THE DATABASE DETAILS SECRATE TO US ONLY and the code should be without double quote so that the Db_Connection_string is in double quote.
 
-# db_connection_string= "mysql+pymysql://vb0x6hpuomu9q7d275xq:pscale_pw_imckS4hZZHfbxmn8kcUVdObckpgKHIe1i3Tr39ElUo@aws.connect.psdb.cloud/careers?charset=utf8mb4"
 engine = create_engine(db_connection_string,
                        connect_args={"ssl": {
                            "ssl ca": "/etc/ssl/cert.pem"
                        }})
-
-
 def load_jobs_from_db():
 
   with engine.connect() as conn:
-
     result = conn.execute(text("select * from jobs"))
-
-    # jobs=[]
-    # for row in result.all():
-    #   jobs.append(row._asdict())
-    # return jobs
     jobs = [row._asdict() for row in result.all()]
     return jobs
 
@@ -38,3 +26,20 @@ def load_job_from_db(id):
     else:
       jobs = [row._asdict() for row in rows]
       return jobs[0]
+
+
+def add_application_to_db(job_id,data):
+  with engine.connect() as conn:
+    query = text("INSERT INTO applications(job_id,full_name,email,linkedin_url,education,work_experience,resume_url) VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)")
+    conn.execute(query, {
+        "job_id": job_id,
+        "full_name": data['full_name'],
+        "email": data['email'],
+        "linkedin_url": data['linkedin_url'],
+        "education": data['education'],
+        "work_experience": data['work_experience'],
+        "resume_url": data['resume_url']
+    })
+
+
+         
